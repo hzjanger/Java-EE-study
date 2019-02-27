155b455bf51fc337919edff2d6676a001bad0a2d
hzjanger <1305696249@qq.com>
2019-02-27 14:16:41 +0800

    ch2 first commit

--- mych1/src/main/java/com/hzj/ioc/demo1/Demo1.java
+++ mych1/src/main/java/com/hzj/ioc/demo1/Demo1.java
@@ -7,7 +7,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Demo1 {
     @Test
     public void demo1() {
+        //创建ApplicationContext容器,
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //通过bean的id值获取进行对象的实例化
         UserService userService = (UserService) applicationContext.getBean("userService");
         userService.say();
     }
